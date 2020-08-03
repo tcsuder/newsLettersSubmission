63e217b55bc64a8ebab5d77c1f8eb038df067672
tcsuder@gmail.com <tcsuder@gmail.com>
2020-08-03 10:07:00 -0700

    set up basic ui orginization

---
+++ src/NewsLetters.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewsLetters.css'
+
+function NewsLetters() {
+  return (
+    <div className="newsLetters">
+      <div className="modal"></div>
+    </div>
+  )
+}
+
+export default NewsLetters
